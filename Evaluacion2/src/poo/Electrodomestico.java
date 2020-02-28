3920795bbe0097bd20c4baeeed2b876ee1c7aa2a
Cristianolivas <colivassoto@gmail.com>
2020-02-27 19:55:31 -0600

    Evaluacion2

---
+++ Evaluacion2/src/poo/Electrodomestico.java
@@ -0,0 +1,32 @@
+package poo;
+
+public class Electrodomestico {
+	
+	int precioBase;
+	
+	int peso;
+	
+	String color;
+	
+	String consumoEnergetico;
+
+	
+	
+	public Electrodomestico() {
+		
+		precioBase=100;
+		
+		peso=5;
+		
+	}
+		
+	}
+		
+
+	
+	
+	
+	
+	
+
+
